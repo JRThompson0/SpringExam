7f2305d2efef7d8a2ff6fff072e17541fb06a760
JRThompson <JRThompson@wil.yearup.org>
2024-06-21 13:28:04 -0400

    initcomit

---
+++ src/test/java/com/cohort15/SpringExam/SpringExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cohort15.SpringExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
